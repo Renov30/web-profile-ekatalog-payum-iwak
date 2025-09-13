d55d0965cd461be535aaf6baa06e37c4159705f9
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-09-13 09:10:04 +0900

    feat(data): new data card and table

--- resources/views/components/data-table.blade.php
+++ resources/views/components/data-table.blade.php
@@ -1,14 +1,22 @@
 <tr>
     <td>
-      <img src="{{Storage::url($data->thumbnail)}}" />
+        <img src="{{ Storage::url($data->thumbnail) }}" />
     </td>
-    <td><p class="nama-petani">Lahan Pak {{$data->name}}</p></td>
-    <td><p>{{$data->name}}</p></td>
-    <td><p>{{$data->distrik->name}}</p></td>
-    <td><p>{{$data->alamat}}</p></td>
     <td>
-      <p>
-        <a href="{{route('front.detail', $data->slug)}}"><button>Lihat Detail</button></a>
-      </p>
+        <p class="nama-petani">Produk Pak {{ $data->name }}</p>
     </td>
-  </tr>
+    <td>
+        <p>{{ $data->name }}</p>
+    </td>
+    <td>
+        <p>{{ $data->kategoriproduk->name }}</p>
+    </td>
+    <td>
+        <p>{{ $data->alamat }}</p>
+    </td>
+    <td>
+        <p>
+            <a href="{{ route('front.detail', $data->slug) }}"><button>Lihat Detail</button></a>
+        </p>
+    </td>
+</tr>
