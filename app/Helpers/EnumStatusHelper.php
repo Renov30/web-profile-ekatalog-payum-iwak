1838179a086ef6a259960b32dd6abb575c5d72c7
Novgeny Ramadhalero Ermiawan <dalero33ermiawan@gmail.com>
2025-09-14 19:32:50 +0900

    feat(helper): adjust status helper

--- app/Helpers/EnumStatusHelper.php
+++ app/Helpers/EnumStatusHelper.php
@@ -11,8 +11,13 @@ class EnumStatusHelper
     {
         $type = DB::select("SHOW COLUMNS FROM {$table} WHERE Field = '{$column}'")[0]->Type;
 
-        preg_match("/^enum\(\'(.*)\'\)$/", $type, $matches);
-        $enum = str_getcsv($matches[1], ',', "'");
+        // Ambil isi dalam kurung enum(...)
+        preg_match("/^enum\((.*)\)$/", $type, $matches);
+
+        // Pecah berdasarkan koma, lalu trim tanda petik
+        $enum = array_map(function ($value) {
+            return trim($value, "'");
+        }, explode(',', $matches[1]));
 
         return array_combine($enum, array_map(fn($v) => Str::ucfirst($v), $enum));
     }
